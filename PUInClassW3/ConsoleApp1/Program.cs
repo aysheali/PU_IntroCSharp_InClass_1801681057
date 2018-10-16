a54584e8af4a6a2b29fbd7185643a165e8910474
Anii998 <anipetrova998@gmail.com>
2018-10-16 18:36:04 +0300

    My new music player.

--- PUInClassW3/ConsoleApp1/Program.cs
+++ PUInClassW3/ConsoleApp1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Media;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,33 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello");
+            Console.WriteLine("Do you want to play some music?");
+            string answer = Console.ReadLine();
+
+            if (answer == "yes")
+            {
+                Console.WriteLine("Playing music.....!!!");
+
+                using (SoundPlayer player = new SoundPlayer(@"C:\Users\fmi\Desktop\week3\music\Mahmut_Orhan_Colonel_Bagshot_-_6_Days_Official_Video_Ultra_Music-1W5BA0lDVLM.wma"))
+                {
+                    // Use PlaySync to load and then play the sound.
+                    // ... The program will pause until the sound is complete.
+                    player.PlaySync();
+                }
+
+                }
+            else if (answer == "no")
+            {
+                Console.WriteLine("Goodbye!");
+            } 
+
+            else
+            {
+                Console.WriteLine("Wrong input!");
+            }
+
+
+            //Console.WriteLine("Hello Ayshe");
         }
     }
 }
